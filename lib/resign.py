533fa6995c6930e22b3ab2d6885ce9843345b6a2
FeTetra <fetetra@protonmail.com>
2024-06-12 16:37:46 -0400

    Implement decrypting EBOOT and debug EBOOT in resign.py

--- lib/resign.py
+++ lib/resign.py
@@ -1,26 +1,68 @@
+# resign.py
+
 import sys
 import os
 import subprocess
 
 from tkinter import filedialog
 
-SCETOOLDIR = "./bin/scetool.exe"
+BINDIR = "./bin/"
 
 class decryptEboot:
     
-    workingDir = "./"
-    pwshFix = ""
+    global input_file_eboot
+    global workingDir
+    global pwshFix
     
+    # Set app working directory and use "powershell" command to run application in Powershell (if Windows is detected)
+    workingDir = "./"
+    if (os.name == "nt"):
+        pwshFix = "powershell "
+        
     # Get EBOOT.bin from tkinter file dialog
     input_file_eboot = str(" \'" + filedialog.askopenfilename(initialdir = workingDir, title = "Select EBOOT.BIN", filetypes = (("BIN Files", "*.bin"), ("All Files", "*.*"))) + "\'")
     
-    if (os.name == "nt"):
-        pwshFix = "powershell "
-
-    print(input_file_eboot)
+    # Get details on the EBOOT
+    def getEbootAttributes():
+        global isDebugEboot
     
-    arg1 = str(pwshFix + SCETOOLDIR + " -i" + input_file_eboot)
-    print(arg1)
+        args = " -i" + input_file_eboot
+        os.system(pwshFix + BINDIR + "scetool" + args + "> EBOOTATTR.txt")
+        
+        with open("EBOOTATTR.txt", 'r') as ebootAttrs:
+            checkAttr = ebootAttrs.read()
+            
+            if ("[DEBUG]") in checkAttr: isDebugEboot = True 
+            else: isDebugEboot = False
+            
+        print(isDebugEboot)
+        
+    # Function for decrypting retail EBOOT
+    def decryptEbootRetail():
+        global cmd
+        
+        if isDebugEboot == True:
+            print("This appears to be a debug EBOOT. Choose the \"Decrypt Debug EBOOT\" option to decrypt this binary.")
+            exit()
+        
+        args = " -v -d " + input_file_eboot + " " + workingDir + "EBOOT.ELF"
+        cmd = str(pwshFix + BINDIR + "scetool" + args)
+        
+    # Function for decrypting debug EBOOT
+    def decryptEbootDebug():
+        global cmd
+        
+        if isDebugEboot == False:
+            print("This appears to be a retail EBOOT. Choose the \"Decrypt Retail EBOOT\" option to decrypt this binary.")
+            exit()
+        
+        args = "" + input_file_eboot + " " + workingDir + "EBOOT.ELF"
+        cmd = str(pwshFix + BINDIR + "unfself" + args)
     
-    os.system(arg1)
-    
+    if not input_file_eboot == " \'\'":
+        getEbootAttributes()
+        decryptEbootDebug()
+        #decryptEbootRetail()
+        os.system(cmd)
+    else:
+        print("It appears no EBOOT file was selected. Please select an EBOOT file in order to decrypt.")
